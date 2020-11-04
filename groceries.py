e05072f08ef072f4dfcb7b79581cf5b593888f07
salfaris <sal.ahmadfaris@gmail.com>
2020-11-04 18:46:33 +0000

    UPDATE: Factored out plotting preambles and added mode for update and plotting consecutively.

--- groceries.py
+++ groceries.py
@@ -31,22 +31,32 @@ def main():
         exit(0)
     
     elif argv[1] == '-p':
-        # Name of chart when saving
-        chart_name = datetime.today().strftime("%b%y") + "chart"
-        
-        # Name of directory to save into
-        target_dir = 'charts'
-        
-        # Create directory if not exists
-        if not os.path.exists(target_dir):
-            os.makedirs(target_dir)
-        
-        # Final path to save to
-        target_path = os.path.join(target_dir, chart_name + ".png")
-        
-        save_get_plot(df, target_path)
-        Image.open(target_path).show()
+        plot_df(df)
         exit(0)
+        
+    elif argv[1] == '-up':
+        update_row(df, FILEPATH)
+        plot_df(df)
+        exit(0)
+
+
+def plot_df(df: pd.DataFrame) -> None:
+    # Name of chart when saving
+    chart_name = datetime.today().strftime("%b%y") + "chart"
+    
+    # Name of directory to save into
+    target_dir = 'charts'
+    
+    # Create directory if not exists
+    if not os.path.exists(target_dir):
+        os.makedirs(target_dir)
+    
+    # Final path to save to
+    target_path = os.path.join(target_dir, chart_name + ".png")
+    
+    save_get_plot(df, target_path)
+    Image.open(target_path).show()
+    exit(0)
     
 if __name__ == '__main__':
     main()
