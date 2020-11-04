151a15dc9819621e35c703c2686f10a9247f236b
salfaris <sal.ahmadfaris@gmail.com>
2020-11-04 13:46:10 +0000

    DOCS: Added documentation

--- helpers.py
+++ helpers.py
@@ -10,8 +10,8 @@ def gbp(str_gbp: str) -> str:
     """
     return "£{:.2f}".format(str_gbp)
 
-
 def get_month_year(dt: datetime) -> str:
+    """Returns the month year format of a datetime object."""
     return dt.strftime("%b %Y")
 
 def get_now_month_year() -> str:
