f69baeb7ccd263bdbb5d333cf71edde4cd56e4a5
salfaris <sal.ahmadfaris@gmail.com>
2020-11-04 13:20:50 +0000

    UPDATE: Removed function

--- helpers.py
+++ helpers.py
@@ -15,13 +15,4 @@ def get_month_year(dt: datetime) -> str:
     return dt.strftime("%b %Y")
 
 def get_now_month_year() -> str:
-    return get_month_year(datetime.today())
-
-def ema(prices: List[float], span: int) -> List[float]:
-    """Compute the EMA of a price list."""
-    factor = 2 / float(span+1)
-    ema_prices = [prices.pop(0)]
-    for idx, price in enumerate(prices):
-        current_ema = price*factor + ema_prices[idx]*(1-factor)
-        ema_prices.append(current_ema)
-    return ema_prices
+    return get_month_year(datetime.todawy())
